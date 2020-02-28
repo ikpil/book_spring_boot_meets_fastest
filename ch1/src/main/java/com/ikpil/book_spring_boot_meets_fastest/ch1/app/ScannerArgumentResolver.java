b138563746a507cf07355324387ff5d8b8c9f131
ikpil <ikpil@naver.com>
2020-02-29 02:48:39 +0900

    챕터1 추가

---
+++ ch1/src/main/java/com/ikpil/book_spring_boot_meets_fastest/ch1/app/ScannerArgumentResolver.java
@@ -0,0 +1,18 @@
+package com.ikpil.book_spring_boot_meets_fastest.ch1.app;
+
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+@Component
+public class ScannerArgumentResolver implements ArgumentResolver {
+    @Override
+    public Argument resolve(InputStream stream) {
+        var scanner = new Scanner(stream);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        return new Argument(a, b);
+    }
+}
