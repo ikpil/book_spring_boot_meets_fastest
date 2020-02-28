e943cbeb48b0aea9fe1de340ab61ae048f1d2d8b
ikpil <ikpil@naver.com>
2020-02-29 03:07:01 +0900

    added - save 할 때, AUTO 키인 ID 받아 오기

--- ch2/src/main/java/com/ikpil/book_spring_boot_meets_fastest/ch2/repository/CustomerRepository.java
+++ ch2/src/main/java/com/ikpil/book_spring_boot_meets_fastest/ch2/repository/CustomerRepository.java
@@ -2,13 +2,16 @@ package com.ikpil.book_spring_boot_meets_fastest.ch2.repository;
 
 import com.ikpil.book_spring_boot_meets_fastest.ch2.domain.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Objects;
 
@@ -23,6 +26,15 @@ public class CustomerRepository {
     @Autowired
     NamedParameterJdbcTemplate jdbcTemplate;
 
+    SimpleJdbcInsert insert;
+
+    @PostConstruct
+    public void init() {
+        insert = new SimpleJdbcInsert((JdbcTemplate) jdbcTemplate.getJdbcOperations())
+                .withTableName("customers")
+                .usingGeneratedKeyColumns("id");
+    }
+
     public List<Customer> findAll() {
         return jdbcTemplate.query("SELECT * FROM `customers` ORDER BY `id`", customerRowMapper);
     }
@@ -35,7 +47,8 @@ public class CustomerRepository {
     public Customer save(Customer customer) {
         var param = new BeanPropertySqlParameterSource(customer);
         if (Objects.isNull(customer.getId())) {
-            jdbcTemplate.update("INSERT INTO `customers` (`first_name`, `last_name`) VALUES (:firstName, :lastName)", param);
+            Number key = insert.executeAndReturnKey(param);
+            customer.setId(key.intValue());
         } else {
             jdbcTemplate.update("UPDATE `customers` SET `first_name` = :firstName, `last_name` = :lastName WHERE `id` = :id", param);
         }
