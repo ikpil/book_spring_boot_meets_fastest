707868f28e70ce8511fef2dbc6d5f459315877ab
ikpil <ikpil@naver.com>
2020-02-29 10:25:55 +0900

    fixed - 페이징 개념 도입

--- ch2/src/main/java/com/ikpil/book_spring_boot_meets_fastest/ch2/App.java
+++ ch2/src/main/java/com/ikpil/book_spring_boot_meets_fastest/ch2/App.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.PageRequest;
 
 @SpringBootApplication
 @ComponentScan
@@ -23,6 +24,13 @@ public class App implements CommandLineRunner {
         var created = repository.save(new Customer(null, "밀란", "쿤데라"));
         System.out.println(created + " is created!");
 
-        repository.findAll().forEach(System.out::println);
+        var pageable = PageRequest.of(0, 3);
+        var page = repository.findAll(pageable);
+        System.out.println("한 페이지당 데이터 수 = " + page.getSize());
+        System.out.println("현재 페이지 = " + page.getNumber());
+        System.out.println("전체 페이지 수 = " + page.getTotalPages());
+        System.out.println("전체 데이터 수 = " + page.getTotalElements());
+
+        page.getContent().forEach(System.out::println);
     }
 }
