9353172d73981d9edb6776dd946a36873d265777
ikpil <ikpil@naver.com>
2020-03-01 10:46:44 +0900

    add - 고객 서비스 클래스 추가

--- ch3/src/main/java/com.ikpil.book_spring_boot_meets_fastest.ch3/service/CustomerService.java
+++ ch3/src/main/java/com.ikpil.book_spring_boot_meets_fastest.ch3/service/CustomerService.java
@@ -3,20 +3,34 @@ package com.ikpil.book_spring_boot_meets_fastest.ch3.service;
 import com.ikpil.book_spring_boot_meets_fastest.ch3.domain.Customer;
 import com.ikpil.book_spring_boot_meets_fastest.ch3.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
-@Service
+@Transactional
 public class CustomerService {
     @Autowired
-    CustomerRepository customerRepository;
-
-    public Customer save(Customer customer) {
-        return customerRepository.save(customer);
-    }
+    CustomerRepository repository;
 
     public List<Customer> findAll() {
-        return customerRepository.findAll();
+        return repository.findAllOrderByName();
     }
+
+    public Optional<Customer> findOne(Integer id) {
+        return repository.findById(id);
+    }
+
+    public Customer create(Customer customer) {
+        return repository.save(customer);
+    }
+
+    public Customer update(Customer customer) {
+        return repository.save(customer);
+    }
+
+    public void delete(Integer id) {
+        repository.deleteById(id);
+    }
+
 }
