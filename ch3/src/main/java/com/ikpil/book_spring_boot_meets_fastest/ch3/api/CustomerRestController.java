1328ff557fc9d61ed011d777651702ecb0f183a3
ikpil <ikpil@naver.com>
2020-03-03 10:03:30 +0900

    add - api/customers 컨트롤러에 POST/PUT/UPDATE/DELETE API 추가 및 실행 스크립트 추가

--- ch3/src/main/java/com/ikpil/book_spring_boot_meets_fastest/ch3/api/CustomerRestController.java
+++ ch3/src/main/java/com/ikpil/book_spring_boot_meets_fastest/ch3/api/CustomerRestController.java
@@ -3,26 +3,44 @@ package com.ikpil.book_spring_boot_meets_fastest.ch3.api;
 import com.ikpil.book_spring_boot_meets_fastest.ch3.domain.Customer;
 import com.ikpil.book_spring_boot_meets_fastest.ch3.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("api/customers")
+@RestController // REST 웹 서비스의 엔드 포인트 컨트롤러 클래스에 지정하는 애너테이션
+@RequestMapping("api/customers")  // REST 웹 서비스의 URL과 매핑 애너테이션
 public class CustomerRestController {
     @Autowired
     CustomerService customerService;
 
     @RequestMapping(method = RequestMethod.GET)
+        // REST API 중 GET 과 매핑
     List<Customer> getCustomers() {
         return customerService.findAll();
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.GET)
+        // REST API 중 ../{id} 로 매핑
     Customer getCustomer(@PathVariable Integer id) {
         return customerService.findOne(id).orElse(null);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    Customer postCustomers(@RequestBody Customer customer) {
+        return customerService.create(customer);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    Customer postCustomer(@PathVariable Integer id, @RequestBody Customer customer) {
+        customer.setId(id);
+        return customerService.update(customer);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void postCustomer(@PathVariable Integer id) {
+        customerService.delete(id);
     }
 }
