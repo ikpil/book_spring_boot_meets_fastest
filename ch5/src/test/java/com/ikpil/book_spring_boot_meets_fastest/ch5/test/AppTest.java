a5b3e52a51b46c14117bdafc8f59241f27dd0dd6
ikpil <ikpil@naver.com>
2020-03-28 23:56:44 +0900

    fixed - ...

--- ch5/src/test/java/com/ikpil/book_spring_boot_meets_fastest/ch5/test/AppTest.java
+++ ch5/src/test/java/com/ikpil/book_spring_boot_meets_fastest/ch5/test/AppTest.java
@@ -1,4 +1,9 @@
 package com.ikpil.book_spring_boot_meets_fastest.ch5.test;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 public class AppTest {
+    int port;
 }
